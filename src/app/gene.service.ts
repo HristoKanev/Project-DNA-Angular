3447c1a7ee2d02f98082d381abfaebe517a43158
HristoKanev <icko_kanev@abv.bg>
2022-07-27 20:18:31 +0300

    connected with django

--- src/app/gene.service.ts
+++ src/app/gene.service.ts
@@ -3,6 +3,9 @@ import {Gene} from './gene';
 import { GENES } from './mock-genes';
 import {Observable, of} from 'rxjs';
 import { MessageService } from './message.service.spec';
+import { HttpClient } from '@angular/common/http';
+import {throwError} from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +14,21 @@ export class GeneService {
   
 
   getGenes(): Observable<Gene[]>{
-    const genes = of(GENES);
+    const genes = this.httpClient.get("http://127.0.0.1:8000/genes").pipe(
+      map(data => data as Gene[]),
+      catchError(error => throwError("Genes not found"))
+    );
     this.messageService.add('GeneService fetched genes')
     return genes;
   }
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private httpClient: HttpClient) { }
   getGene(id: number): Observable<Gene> {
     
-    const gene = GENES.find(h => h.id === id)!;
+    const gene =  this.httpClient.get(`http://127.0.0.1:8000/genes/${id}`).pipe(
+      map(data => data as Gene),
+      catchError(error => throwError(`Gene with ${id} not found`))
+    );
     this.messageService.add(`GeneService: fetched gene id=${id}`);
-    return of(gene);
+    return gene;
   }
 }
